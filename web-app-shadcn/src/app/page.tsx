c36640d56919c21616d2d9cec0968dbcf68ff409
aericio <16523741+Aericio@users.noreply.github.com>
2023-11-03 21:16:20 -1000

    feat: add prettier

--- web-app-shadcn/src/app/page.tsx
+++ web-app-shadcn/src/app/page.tsx
@@ -1,22 +1,29 @@
-import data from '@/public/data.json'
-import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion";
+import data from "@/public/data.json";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Home() {
-    // TODO: fetch data from an API
-    const askus = data as Record<string, Record<string, string>>;
+  // TODO: fetch data from an API
+  const askus = data as Record<string, Record<string, string>>;
 
-    return (
-        <main className={"relative p-3"}>
-            <Accordion type="single" collapsible className={"w-full"}>
-                {Object.keys(askus).map((id, index) => {
-                    return (
-                        <AccordionItem key={index} value={`question-${id}`}>
-                            <AccordionTrigger>Question {id} - {askus[id].question}</AccordionTrigger>
-                            <AccordionContent>{askus[id].answer}</AccordionContent>
-                        </AccordionItem>
-                    )
-                })}
-            </Accordion>
-        </main>
-    )
+  return (
+    <main className={"relative p-3"}>
+      <Accordion type="single" collapsible className={"w-full"}>
+        {Object.keys(askus).map((id, index) => {
+          return (
+            <AccordionItem key={index} value={`question-${id}`}>
+              <AccordionTrigger>
+                Question {id} - {askus[id].question}
+              </AccordionTrigger>
+              <AccordionContent>{askus[id].answer}</AccordionContent>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
+    </main>
+  );
 }
