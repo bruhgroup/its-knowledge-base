eee9bb30d60447ee41e76e58c203a49989ce6b5f
echung32 <143231894+echung32@users.noreply.github.com>
2023-11-03 21:16:20 -1000

    feat: add prettier

--- web-app-shadcn/src/app/chatbox/page.tsx
+++ web-app-shadcn/src/app/chatbox/page.tsx
@@ -1,5 +1,5 @@
 import ChatWindow from "@/components/ChatWindow";
 
 export default function ChatboxPage() {
-    return <ChatWindow/>
+  return <ChatWindow />;
 }
--- web-app-shadcn/src/components/ChatWindow.tsx
+++ web-app-shadcn/src/components/ChatWindow.tsx
@@ -1,51 +1,64 @@
-"use client"
+"use client";
 
-import {zodResolver} from "@hookform/resolvers/zod"
-import {useForm} from "react-hook-form"
-import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
-import {Button} from "@/components/ui/button"
-import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form"
-import {Textarea} from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
 
 const FormSchema = z.object({
-    question: z.string(),
-})
+  question: z.string(),
+});
 
 export default function TextareaForm() {
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
-    })
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
 
-    function onSubmit(data: z.infer<typeof FormSchema>) {
-        console.log(data);
-    }
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="container w-full p-3 space-y-6">
-                <FormField
-                    control={form.control}
-                    name="question"
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>Need help?</FormLabel>
-                            <FormControl>
-                                <Textarea
-                                    placeholder="Ask a question"
-                                    className="resize-none"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormDescription>
-                                Your messages may be monitored and reviewed.
-                            </FormDescription>
-                            <FormMessage/>
-                        </FormItem>
-                    )}
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="container w-full p-3 space-y-6"
+      >
+        <FormField
+          control={form.control}
+          name="question"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Need help?</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Ask a question"
+                  className="resize-none"
+                  {...field}
                 />
-                <Button type="submit" className={"w-full"}>Ask Question</Button>
-            </form>
-        </Form>
-    )
+              </FormControl>
+              <FormDescription>
+                Your messages may be monitored and reviewed.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className={"w-full"}>
+          Ask Question
+        </Button>
+      </form>
+    </Form>
+  );
 }
