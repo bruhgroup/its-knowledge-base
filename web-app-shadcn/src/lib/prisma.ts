2413d355f0cc954d4dca11394c710f28767c4d1e
echung32 <143231894+echung32@users.noreply.github.com>
2023-11-09 01:57:55 -1000

    feat: update users/questions on form submit
    Took 1 hour 24 minutes

--- web-app-shadcn/src/lib/prisma.ts
+++ web-app-shadcn/src/lib/prisma.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
+// image bitnami/postgresql
+// POSTGRESQL_DATABASE = next
+// POSTGRESQL_USERNAME = next
+// POSTGRESQL_PASSWORD = next1234
+
 const prisma: PrismaClient = new PrismaClient();
 
 export default prisma;
