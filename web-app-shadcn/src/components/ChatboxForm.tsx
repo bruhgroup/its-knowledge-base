edbbcaa9fa0c0d2078bc2d0219a5938e0bcd633b
echung32 <143231894+echung32@users.noreply.github.com>
2023-11-09 21:23:36 -1000

    refactor: popup modal css positioning

--- web-app-shadcn/src/components/ChatboxForm.tsx
+++ web-app-shadcn/src/components/ChatboxForm.tsx
@@ -44,65 +44,70 @@ export default function ChatboxForm() {
 
   // TODO: Make a different form page for ask question.
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit((data) => handleChatboxSubmission(data))}
-        className="container w-full p-3 space-y-4"
-      >
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input
-                  type={"email"}
-                  placeholder={"email@hawaii.edu"}
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="firstName"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>First Name</FormLabel>
-              <FormControl>
-                <Input type={"text"} placeholder={"John"} {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="question"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Ask your question</FormLabel>
-              <FormControl>
-                <Textarea
-                  placeholder="I need help with..."
-                  className="resize-none"
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription>
-                Your messages may be monitored and reviewed.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className={"w-full"}>
-          Ask Question
-        </Button>
-      </form>
-    </Form>
+    <div className={"bg-white"}>
+      <div className={"bg-blue-500 h-[3em] flex items-center justify-center"}>
+        <span className={"font-semibold text-white"}>How can we help you?</span>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit((data) => handleChatboxSubmission(data))}
+          className={"container w-full p-3 space-y-4"}
+        >
+          <FormField
+            control={form.control}
+            name={"email"}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type={"email"}
+                    placeholder={"email@hawaii.edu"}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name={"firstName"}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>First Name</FormLabel>
+                <FormControl>
+                  <Input type={"text"} placeholder={"John"} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name={"question"}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Ask your question</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder={"I need help with..."}
+                    className={"resize-none"}
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Your messages may be monitored and reviewed.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type={"submit"} className={"w-full"}>
+            Ask Question
+          </Button>
+        </form>
+      </Form>{" "}
+    </div>
   );
 }
--- web-app-shadcn/src/components/ChatboxState.tsx
+++ web-app-shadcn/src/components/ChatboxState.tsx
@@ -2,15 +2,30 @@
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import ChatboxModal from "@/components/ChatboxModal";
+import ChatboxForm from "@/components/ChatboxForm";
 
 export default function ChatboxState() {
   const [chatbox, setChatbox] = useState<boolean>(false);
 
-  if (chatbox) return <ChatboxModal closeModal={() => setChatbox(!chatbox)} />;
   return (
-    <Button type={"button"} onClick={() => setChatbox(!chatbox)}>
-      Ask Question
-    </Button>
+    <div className={"absolute bottom-4 right-8 flex flex-col gap-3 items-end"}>
+      {chatbox && (
+        <div className={"shadow-2xl"}>
+          <ChatboxForm />
+          {/*<iframe*/}
+          {/*  src={"/chatbox"}*/}
+          {/*  allowFullScreen*/}
+          {/*  className={"w-full h-full border-none"}*/}
+          {/*/>*/}
+        </div>
+      )}
+      <Button
+        type={"button"}
+        onClick={() => setChatbox(!chatbox)}
+        className={"shadow-2xl max-w-fit"}
+      >
+        Contact Us
+      </Button>
+    </div>
   );
 }
