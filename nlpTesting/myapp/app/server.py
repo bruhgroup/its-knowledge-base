558db0e1aa26f33a8f773acb6af7ece7a673ae87
Vincent <95856449+eve-liya@users.noreply.github.com>
2023-11-13 13:16:37 -1000

    feat(langchan): follow llama prompt template

--- nlpTesting/myapp/app/server.py
+++ nlpTesting/myapp/app/server.py
@@ -2,13 +2,12 @@ import json
 from typing import List, Tuple
 
 from fastapi import FastAPI
-from langchain.chains import ConversationalRetrievalChain
 from langchain.docstore.document import Document
 from langchain.embeddings import GPT4AllEmbeddings, CacheBackedEmbeddings
 from langchain.globals import set_debug
 from langchain.globals import set_verbose
 from langchain.llms import LlamaCpp
-from langchain.prompts import HumanMessagePromptTemplate, SystemMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import  PromptTemplate
 from langchain.pydantic_v1 import BaseModel, Field
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough, RunnableParallel
@@ -62,18 +61,30 @@ If you don't know the answer, just say that you don't know, don't try to make up
 ----------------
 {context}"""
 
-messages = [SystemMessagePromptTemplate.from_template(SYSTEM_TEMPLATE),
-            HumanMessagePromptTemplate.from_template("[INST]{question}[/INST]")]
+# messages = [SystemMessagePromptTemplate.from_template(SYSTEM_TEMPLATE),
+#             HumanMessagePromptTemplate.from_template("[INST]{question}[/INST]")]
 
-prompt = ChatPromptTemplate.from_messages(messages)
+TEMPLATE = """<<SYS>> You are a helpful, respectful and honest assistant. If a question does not make any sense, 
+or is not factually coherent, explain why instead of answering something not correct. If you are unsure about an 
+answer, truthfully say "I don't know".
+
+Here are some relevant documents that you can refer to:
+{context}<</SYS>>
+[INST]Respond to the following question with step-by-step instructions to assist the user.[/INST]
+User: {question}
+"""
+
+prompt = PromptTemplate.from_template(TEMPLATE)
+# prompt = ChatPromptTemplate.from_messages(messages)
 
 # Define Model
 model = LlamaCpp(
     model_path=MODEL_FILEPATH,
-    max_tokens=1024,
+    max_tokens=256,
     n_ctx=4096,
+    n_batch=2048,
     n_threads=10,
-    n_gpu_layers=40,
+    n_gpu_layers=50,
     f16_kv=True,  # MUST set to True, otherwise you will run into problem after a couple of calls
     verbose=True,  # Verbose is required to pass to the callback manager
 )
@@ -91,12 +102,9 @@ class ChatHistory(BaseModel):
 
 
 # https://python.langchain.com/docs/modules/data_connection/document_transformers/text_splitters/split_by_token
-# Splitting by character may result in some loss of context? Maybe worth attempting to split by tokens.
 text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-    # Set a really small chunk size, just to show.
     chunk_size=1000,
     chunk_overlap=200,
-
 )
 
 # Embed the text and cache it
@@ -110,8 +118,7 @@ documents = text_splitter.split_documents(load_json_file("../../resources/json/d
 
 # Store the embedded chunks into a vector store for easy access
 vectorstore = Chroma.from_documents(documents, cached_embedder)
-# retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
-retriever = vectorstore.as_retriever()
+retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
 
 # chain = ConversationalRetrievalChain.from_llm(model, retriever, prompt).with_types(
 #     input_type=ChatHistory
@@ -145,55 +152,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-# Test get with url parameter
-@app.get("/help/{question}")
-async def root(question):
-    a = (
-            {
-                "context": retriever | format_docs,
-                "question": RunnablePassthrough()
-            }
-            | prompt
-            | model
-            | StrOutputParser()
-    )
-    return f'{a.invoke(question)}\n\nSources: {", ".join(sources)}'
-
-
-#
-#
-# @app.post("/test/invoke")
-# async def work(query: Question):
-#     a = (
-#             {
-#                 "context": retriever | format_docs,
-#                 "question": RunnablePassthrough()
-#             }
-#             | prompt
-#             | model
-#             | StrOutputParser()
-#     )
-#     return f'{a.invoke(query)}\n\nSources: {", ".join(sources)}'
-
-
-# #
-# @app.get("/chain/invoke")
-# async def get_chain():
-#     return (
-#             {
-#                 "context": retriever | format_docs,
-#                 "question": RunnablePassthrough()
-#             }
-#             | prompt
-#             | model
-#             | StrOutputParser()
-#     )
-
-
-
-# Model without context
-
 # add_routes(app, retriever | format_docs, path="/retriever")
 add_routes(app, retriever)
 
@@ -204,7 +162,7 @@ class Question(BaseModel):
 
 
 chain = (
-        RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
+        RunnableParallel({"context": retriever | format_docs, "question": RunnablePassthrough()})
         | prompt
         | model
         | StrOutputParser()
