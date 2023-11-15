be237265787d6f3194ec80aee5562db6abb309ce
aericio <16523741+Aericio@users.noreply.github.com>
2023-11-15 00:58:03 -1000

    chore: dedupe code and run formatter

--- web-app-shadcn/src/components/ChatMessage.tsx
+++ web-app-shadcn/src/components/ChatMessage.tsx
@@ -1,8 +1,10 @@
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { ChatMessageType } from "@prisma/client";
+import Image from "next/image";
 
 export default function ChatMessage({
-  type, loaded,
+  type,
+  loaded,
   name,
   text,
 }: {
@@ -29,35 +31,28 @@ export default function ChatMessage({
       </div>
     );
   }
-if (loaded) {
-    return (
-        <div className={"flex items-end gap-2 justify-start"}>
-            <Avatar>
-                <AvatarFallback>ITS</AvatarFallback>
-            </Avatar>
-            <div
-                className={
-                    "bg-gray-300 rounded-e-2xl rounded-tl-2xl p-3 [word-break:break-word]"
-                }
-            >
-                <p>{text}</p>
-            </div>
-        </div>
-    );
-} else {
-    return (
-        <div className={"flex items-end gap-2 justify-start"}>
-            <Avatar>
-                <AvatarFallback>ITS</AvatarFallback>
-            </Avatar>
-            <div
-                className={
-                    "bg-gray-300 rounded-e-2xl rounded-tl-2xl p-3 [word-break:break-word]"
-                }
-            >
-            <img src={"https://raw.githubusercontent.com/n3r4zzurr0/svg-spinners/abfa05c49acf005b8b1e0ef8eb25a67a7057eb20/svg-css/3-dots-bounce.svg"}/>
-            </div>
-        </div>
-    );
-}
+
+  return (
+    <div className={"flex items-end gap-2 justify-start"}>
+      <Avatar>
+        <AvatarFallback>ITS</AvatarFallback>
+      </Avatar>
+      <div
+        className={
+          "bg-gray-300 rounded-e-2xl rounded-tl-2xl p-3 [word-break:break-word]"
+        }
+      >
+        {loaded ? (
+          <p>{text}</p>
+        ) : (
+          <Image
+            src={
+              "https://raw.githubusercontent.com/n3r4zzurr0/svg-spinners/abfa05c49acf005b8b1e0ef8eb25a67a7057eb20/svg-css/3-dots-bounce.svg"
+            }
+            alt={"Getting response..."}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
