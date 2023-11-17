81deb035f57ee478812034c56301e44a561a5cfe
aericio <16523741+Aericio@users.noreply.github.com>
2023-11-16 17:43:45 -1000

    feat(auth): use grid for page layout

--- web-app-shadcn/src/app/authentication/page.tsx
+++ web-app-shadcn/src/app/authentication/page.tsx
@@ -12,40 +12,43 @@ export const metadata: Metadata = {
 
 export default function AuthenticationPage() {
   return (
-    <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-        <div className="absolute inset-0 bg-zinc-900" />
-        <div className={"absolute left-[10%] top-[40%]"}>
-          <Image src={itsLogo} alt={"its-logo"} height={200} />
-        </div>
+    <div
+      className={
+        "h-screen w-full grid lg:grid-cols-2 lg:grid-rows-1 grid-cols-1 grid-rows-2 justify-items-center items-center"
+      }
+    >
+      <div
+        className={
+          "bg-slate-900 h-full w-full flex justify-center items-center"
+        }
+      >
+        <Image src={itsLogo} alt={"its-logo"} />
       </div>
-      <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
-            <p className="text-sm text-muted-foreground">
-              Log in with your Google account
-            </p>
-          </div>
-          <UserAuthForm />
-          <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our{" "}
-            <Link
-              href="/terms"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Terms of Service
-            </Link>{" "}
-            and{" "}
-            <Link
-              href="/privacy"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Privacy Policy
-            </Link>
-            .
+      <div className="flex h-1/2 w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
+          <p className="text-sm text-muted-foreground">
+            Log in using your Google account
           </p>
         </div>
+        <UserAuthForm />
+        <p className="px-8 text-center text-sm text-muted-foreground">
+          By clicking continue, you agree to our{" "}
+          <Link
+            href="/terms"
+            className="underline underline-offset-4 hover:text-primary"
+          >
+            Terms of Service
+          </Link>{" "}
+          and{" "}
+          <Link
+            href="/privacy"
+            className="underline underline-offset-4 hover:text-primary"
+          >
+            Privacy Policy
+          </Link>
+          .
+        </p>
       </div>
     </div>
   );
