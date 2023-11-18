3a9299a165845fcf77bb74646bae822dc3f40cad
aericio <16523741+Aericio@users.noreply.github.com>
2023-11-18 02:50:22 -1000

    feat(embed): add embeddable chatbox page

---
+++ web-app-shadcn/src/app/(embeddable)/layout.tsx
@@ -0,0 +1,19 @@
+import { Inter } from "next/font/google";
+import React from "react";
+import AuthProvider from "@/components/authentication/AuthProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <AuthProvider>
+        <body className={inter.className}>{children}</body>
+      </AuthProvider>
+    </html>
+  );
+}
