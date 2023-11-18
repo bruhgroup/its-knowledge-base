2bd7e3cb95d5d7b7a97cfca452dbd56dca89813c
salina-t <salinat2004@gmail.com>
2023-11-18 01:50:48 -1000

    style: update arrow in history
    Took 44 minutes

--- web-app-shadcn/src/app/(main)/history/[userId]/page.tsx
+++ web-app-shadcn/src/app/(main)/history/[userId]/page.tsx
@@ -41,7 +41,7 @@ export default async function SessionsPage({
               <TableCell>{session.createdAt.toISOString()}</TableCell>
               <TableCell>{session._count.chatMessages}</TableCell>
               <TableCell className="text-right">
-                <a href={`/history/session/${session.id}`}>Arrow</a>
+                <a href={`/history/session/${session.id}`}>🡺</a>
               </TableCell>
             </TableRow>
           ))}
