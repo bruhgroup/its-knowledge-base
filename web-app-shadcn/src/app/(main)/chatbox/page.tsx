fbe8aae03c64e8ffe5ed4521d9f08f894bc5bcf0
aericio <16523741+Aericio@users.noreply.github.com>
2023-11-18 01:46:24 -1000

    feat(chatbox): constrain page size for full-size chatbox

--- web-app-shadcn/src/app/(main)/chatbox/page.tsx
+++ web-app-shadcn/src/app/(main)/chatbox/page.tsx
@@ -1,5 +1,9 @@
 import ChatboxForm from "@/components/ChatboxForm";
 
 export default function ChatboxPage() {
-  return <ChatboxForm />;
+  return (
+    <div className={"border border-green-800 rounded shadow-2xl"}>
+      <ChatboxForm />
+    </div>
+  );
 }
