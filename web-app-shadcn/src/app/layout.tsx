048c88283d1071793e6615c77a93b97d8ba5b64e
salina-t <salinat2004@gmail.com>
2023-11-18 00:58:51 -1000

    style: update navbar color and add arrow
    Took 1 hour 47 minutes

--- web-app-shadcn/src/app/layout.tsx
+++ web-app-shadcn/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
     <html lang="en">
       <AuthProvider>
         <body className={inter.className}>
-          <div className="flex h-16 items-center px-4 border-b">
+          <div className="flex h-20 items-center px-4 border-b bg-green-900">
             <NavigationMenu />
             {session ? (
               <Link
@@ -44,7 +44,7 @@ export default async function RootLayout({
                 href="/auth/login?callbackUrl=/"
                 className={cn(
                   buttonVariants({ variant: "ghost" }),
-                  "ml-auto flex items-center",
+                  "ml-auto flex items-center text-white hover:underline",
                 )}
               >
                 Login
